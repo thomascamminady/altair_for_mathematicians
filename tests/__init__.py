770b0a37b81360eb487e9b2b177cbbafb7e0b6e4
Thomas Camminady <0milieux_member@icloud.com>
2023-03-29 14:02:48 +0200

    Initial commit after initializing the project.

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for altair_for_mathematicians."""
---
+++ tests/test_altair_for_mathematicians.py
@@ -0,0 +1,8 @@
+"""Tests for `altair_for_mathematicians` package."""
+
+
+from altair_for_mathematicians import __version__
+
+
+def test_version() -> None:
+    assert __version__ == "0.1.0"
